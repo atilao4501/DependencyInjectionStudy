cee9a8e0a0189240a0eb5f7ece2f5633b37d9951
atilao4501 <atilafa2012@hotmail.com>
2024-06-17 16:52:46 -0300

    final

--- Program.cs
+++ Program.cs
@@ -1,2 +1,98 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data.Common;
+using Microsoft.Extensions.DependencyInjection;
+
+IServiceCollection collection = new ServiceCollection();
+collection.AddScoped<IDataAcess, DataAcessMySql>();
+collection.AddScoped<IBusiness, BusinessV2>();
+collection.AddScoped<UserInterface>();
+
+IServiceProvider serviceProvider = collection.BuildServiceProvider();
+
+UserInterface ui = serviceProvider.GetService<UserInterface>();
+ui.Signup();
+
+public class UserInterface
+{
+    private string _userName;
+    private string _password;
+
+    private IBusiness _business;
+
+    public UserInterface(IBusiness business)
+    {
+        _business = business;
+    }
+
+    private void GetData()
+    {
+        Console.WriteLine("Enter your username:");
+        _userName = Console.ReadLine();
+
+        Console.WriteLine("Enter your password");
+        _password = Console.ReadLine();
+    }
+
+    // sign up 
+
+    public void Signup()
+    {
+        GetData();
+
+        //var biz = new Business();
+        _business.SignUp(_userName, _password);
+    }
+}
+
+public interface IBusiness
+{
+    public void SignUp(string userName, string password);
+}
+
+public class Business : IBusiness
+{
+    private IDataAcess _dataAcess;
+    public Business(IDataAcess dataAcess)
+    {
+        _dataAcess = dataAcess;
+    }
+    public void SignUp(string username, string password)
+    {
+        _dataAcess.SignUp(username, password);
+
+    }
+}
+
+public class BusinessV2 : IBusiness
+{
+    private IDataAcess _dataAcess;
+
+    public BusinessV2(IDataAcess dataAcess)
+    {
+        _dataAcess = dataAcess;
+    }
+
+    public void SignUp(string username, string password)
+    {
+        _dataAcess.SignUp(username, password);
+    }
+}
+
+public interface IDataAcess
+{
+    void SignUp(string userName, string password);
+}
+
+public class DataAcessSqlServer : IDataAcess
+{
+    public void SignUp(string username, string password)
+    {
+        //Entity Framework
+    }
+}
+public class DataAcessMySql : IDataAcess
+{
+    public void SignUp(string username, string password)
+    {
+        //Entity Framework
+    }
+}
